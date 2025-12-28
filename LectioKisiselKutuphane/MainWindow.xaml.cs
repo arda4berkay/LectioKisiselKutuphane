a245b4f276a8c0e6128e934ac33b7ed81a87b01f
arda4berkay <ardaberkay34811@gmil.com>
2025-12-28 04:39:00 +0300

    .

--- LectioKisiselKutuphane/MainWindow.xaml.cs
+++ LectioKisiselKutuphane/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace LectioKisiselKutuphane
         public MainWindow()
         {
             InitializeComponent();
+            kitapduzenleButton.IsEnabled = false;
+            kitapsilButton.IsEnabled = false;
         }
 
         private void UserDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
